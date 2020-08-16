b2d9e39ea58cf0c626f272f77b9fdfec3f3e92a6
jly <453825818@qq.com>
2020-08-16 13:48:13 +0800

    state

---
+++ 04-todolist/src/App-state.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import './App-style.css';
+
+class App extends Component{
+    //初始化
+    constructor(props){
+        super(props)
+        this.state = {
+            list:['crossfire','LOL','PUBG'],
+            task:''
+        }
+    }
+
+    handAdd(){
+        // console.log('handleClick...');
+        //改变state
+        this.setState({
+            list:[...this.state.list,this.state.task],
+            task:''
+        })
+    }
+
+    handChange(ev){
+        // console.log(ev.target.value);
+        this.setState({
+            task:ev.target.value
+        })
+    }
+
+    handDel(index){
+        const list = this.state.list;
+        list.splice(index,1);
+        this.setState({
+            list:list
+        })
+    }
+    render(){
+        return(
+            <div className="App">
+                <input onChange={this.handChange.bind(this)} value={this.state.task} />
+                <button className="btn" onClick={this.handAdd.bind(this)}>按钮</button>
+                <ul>
+                    {
+                        this.state.list.map((value,index)=>{
+                            return(<li 
+                                key={index}
+                                onClick={this.handDel.bind(this,index)}
+
+                                >{value}
+                            </li>)
+                        })
+                    }
+                </ul>
+            </div>
+        )
+    }
+}
+
+export default App;
